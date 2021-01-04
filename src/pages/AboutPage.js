232ac0ed320f38ddb9c83a65eeb92dfba437caac
122Miller <70303237+122Miller@users.noreply.github.com>
2021-01-04 15:53:44 -0800

    first commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <React.Fragment>
+  <h1>About Me!</h1>
+    <p>
+       This is my about me! 
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis sapien et tellus dapibus rutrum.
+        In interdum ligula ac semper ultricies. Aliquam et scelerisque purus. Vestibulum non tincidunt sem. 
+        Sed fringilla magna in odio dictum, sit amet scelerisque nunc pharetra. Nulla ac elit sit amet nisl aliquet elementum. 
+        Sed leo orci, vehicula quis ultrices sit amet, aliquam eu urna. Duis magna turpis, pharetra at feugiat et, imperdiet nec purus. 
+        Ut blandit ultrices ex. Nunc consequat vestibulum tortor, eu maximus nibh egestas interdum. Aliquam porttitor euismod ipsum, 
+        non aliquet dui consectetur at. Nulla blandit tempus ornare. Vivamus efficitur augue nec iaculis placerat.
+        Nulla ut arcu faucibus, convallis tellus et, interdum erat. Suspendisse vulputate justo nec dignissim aliquam. Nulla 
+        lacinia feugiat risus, at condimentum elit luctus nec.
+    </p>
+    </React.Fragment>
+  
+);
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis sapien et tellus dapibus rutrum.
+                    In interdum ligula ac semper ultricies. Aliquam et scelerisque purus. Vestibulum non tincidunt sem. 
+        Sed fringilla magna in odio dictum, sit amet scelerisque nunc pharetra. Nulla ac elit sit amet nisl aliquet elementum. 
+                Sed leo orci, vehicula quis ultrices sit amet, aliquam eu urna. Duis magna turpis, pharetra at feugiat et, imperdiet nec purus. 
+        Ut blandit ultrices ex. Nunc consequat vestibulum tortor, eu maximus nibh egestas interdum. Aliquam porttitor euismod ipsum, 
+        non aliquet dui consectetur at. Nulla blandit tempus ornare. Vivamus efficitur augue nec iaculis placerat.
+        Nulla ut arcu faucibus, convallis tellus et, interdum erat. Suspendisse vulputate justo nec dignissim aliquam. Nulla 
+        lacinia feugiat risus, at condimentum elit luctus nec.
+    </p>
+    <p>
+        
+        non aliquet dui consectetur at. Nulla blandit tempus ornare. Vivamus efficitur augue nec iaculis placerat.
+        Nulla ut arcu faucibus, convallis tellus et, interdum erat. Suspendisse vulputate justo nec dignissim aliquam. Nulla 
+        lacinia feugiat risus, at condimentum elit luctus nec.
+    </p>
+
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis sapien et tellus dapibus rutrum.
+            In interdum ligula ac semper ultricies. Aliquam et scelerisque purus. Vestibulum non tincidunt sem. 
+            Sed fringilla magna in odio dictum, sit amet scelerisque nunc pharetra. Nulla ac elit sit amet nisl aliquet elementum. 
+            Sed leo orci, vehicula quis ultrices sit amet, aliquam eu urna. Duis magna turpis, pharetra at feugiat et, imperdiet nec purus. 
+            Ut blandit ultrices ex. Nunc consequat vestibulum tortor, eu maximus nibh egestas interdum. Aliquam porttitor euismod ipsum, 
+    </p>
+    
+    </>
+  
+);
+
+export default HomePage;
