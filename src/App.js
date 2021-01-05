effc21d86bc544c0aaa9763baca98d52166a4233
Christian Miller <christian_miller122@hotmail.com>
2021-01-04 22:59:59 -0800

    full website

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
-import 'whatwg-fetch';
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ArticlesListPage from './pages/ArticlesListPage';
-import ArticlePage from './pages/ArticlePage';
-import NavBar from './NavBar';
-import NotFoundPage from './pages/NotFoundPage';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
   render() {
     return (
-      <Router>
-        <div className="App">
-          <NavBar />
-          <div id="page-body">
-            <Switch>
-            <Route path="/" component={HomePage} exact />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesListPage} />
-            <Route path="/article/:name" component={ArticlePage} />
-            <Route component={NotFoundPage} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        {/*<Portfolio data={this.state.resumeData.portfolio}/>*/}
+        {/*<Testimonials data={this.state.resumeData.testimonials}/ >*/}
+        {/*<Contact data={this.state.resumeData.main}/>*/}
+        <Footer data={this.state.resumeData.main}/>
+      </div>
     );
   }
 }
